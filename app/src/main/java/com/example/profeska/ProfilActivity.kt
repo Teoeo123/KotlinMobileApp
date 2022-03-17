83c5a727582bac6e51513d49381c5b47dd6750a2
Teoeo123 <82089406+Teoeo123@users.noreply.github.com>
2022-03-17 13:41:46 +0100

    Edytowanie profilu test

--- app/src/main/java/com/example/profeska/ProfilActivity.kt
+++ app/src/main/java/com/example/profeska/ProfilActivity.kt
@@ -7,5 +7,6 @@ class ProfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profil)
+
     }
 }
