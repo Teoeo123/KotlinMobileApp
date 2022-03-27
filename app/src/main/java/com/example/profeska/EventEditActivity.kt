dd1a668356710f43589c9c759368a44c47171bc8
Teoeo123 <82089406+Teoeo123@users.noreply.github.com>
2022-03-28 01:35:28 +0200

    eventy

--- app/src/main/java/com/example/profeska/EventEditActivity.kt
+++ app/src/main/java/com/example/profeska/EventEditActivity.kt
@@ -1,90 +1,83 @@
 package com.example.profeska
 
-import android.annotation.SuppressLint
 import android.app.ProgressDialog
 import android.content.Intent
-import android.content.UriMatcher
-import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.WindowInsets
-import android.view.WindowManager
-import android.widget.RadioButton
-import android.widget.Toast
+import com.example.profeska.databinding.ActivityEventEditBinding
 import com.example.profeska.databinding.ActivityProfilEditBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
-import java.io.File
-import java.net.URI
-import java.text.SimpleDateFormat
-import java.util.*
 
+class EventEditActivity : AppCompatActivity() {
 
-class ProfilEditActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityProfilEditBinding
+    private lateinit var binding: ActivityEventEditBinding
     private lateinit var myRef: DatabaseReference
+    private lateinit var allRef: DatabaseReference
     private lateinit var user: FirebaseAuth
     private lateinit var imageUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_profil_edit)
 
-
-        binding = ActivityProfilEditBinding.inflate(layoutInflater)
+        binding = ActivityEventEditBinding.inflate(layoutInflater)
         setContentView(binding.root)
         fullScreen(window)
-        var plec ="inne"
 
         val firebase = FirebaseDatabase.getInstance("https://profeska-ad23d-default-rtdb.europe-west1.firebasedatabase.app")
         user = FirebaseAuth.getInstance()
-        myRef = firebase.getReference("users")
-        val id=user.uid
+        myRef = firebase.getReference("events").child("users")
+        allRef = firebase.getReference("events").child("all")
 
-        binding.rgSex.setOnCheckedChangeListener { _, checkedId ->
-                val rb = findViewById<RadioButton>(checkedId)
-                if(rb!=null) {
-                    plec = rb.text.toString()
-                }
-        }
 
-        binding.btnProf.setOnClickListener{
-            val name = binding.etUserName.text.toString()
-            val sName = binding.etUserSName.text.toString()
-            val number = binding.etUserPhon.text.toString()
-            val city = binding.etUserCity.text.toString()
-            val firebaseInput = DatabaseRow(name,sName,number,city,plec)
-            myRef.child(id.toString()).setValue(firebaseInput)
-            if(imageUri!=null) {
-                uploadImage()
-            }
-            startActivity(Intent(this,LogoutActivity::class.java))
-        }
-
-        binding.profPicEdit.setOnClickListener{
-            Log.d("Zdjecie","dziala")
+        binding.imgEdit.setOnClickListener{
             selectImage()
         }
 
+        binding.btnzatw.setOnClickListener {
+            val n=sendToBase()
+            if(imageUri!=null) uploadImage(n)
+            startActivity(Intent(this,LogoutActivity::class.java))
+        }
+
+        binding.btnodr.setOnClickListener {
+            startActivity(Intent(this,LogoutActivity::class.java))
+        }
+
+
     }
 
-    private  fun uploadImage(){
+    private fun sendToBase():String
+    {
+        val id=user.uid
+        val name=binding.eventName.text.toString()
+        val desc=binding.eventDes.text.toString()
+        val slots=binding.eventSlots.text.toString().toInt()
+        val city=binding.eventCity.text.toString()
+        val street=binding.eventStreet.text.toString()
+        val nr=binding.eventNr.text.toString()
+        val photo="$id$name"
+        val data=DatabaseEvent(name,desc,slots,city,street,nr,photo)
+        myRef.child("$id").child("$name").setValue(data)
+        allRef.child("$photo").setValue(data)
+        return name
+    }
+
+    private  fun uploadImage(name:String){
         val progressDialog = ProgressDialog(this)
         progressDialog.setMessage("Upload Image")
         progressDialog.setCancelable(false)
         progressDialog.show()
         val fileName = user.uid
-        val storageRef= FirebaseStorage.getInstance("gs://profeska-ad23d.appspot.com").reference.child("users/$fileName")
+        val storageRef= FirebaseStorage.getInstance("gs://profeska-ad23d.appspot.com").reference.child("events/$fileName$name")
         storageRef.putFile((imageUri))
             .addOnSuccessListener {
 
-                binding.profPicEdit.setImageURI(null)
+                binding.imgEdit.setImageURI(null)
                 Log.d("ADDING","SUCCESS")
             }
             .addOnFailureListener{
@@ -106,9 +99,8 @@ class ProfilEditActivity : AppCompatActivity() {
 
         if (requestCode ==100 && resultCode == RESULT_OK){
             imageUri = data?.data!!
-            binding.profPicEdit.setImageURI(imageUri)
+            binding.imgEdit.setImageURI(imageUri)
             Log.d("TEST","$imageUri")
         }
     }
-
 }
--- app/src/main/java/com/example/profeska/MyAdapter.kt
+++ app/src/main/java/com/example/profeska/MyAdapter.kt
@@ -1,24 +1,15 @@
 package com.example.profeska
 
-import android.content.Intent
-import android.content.Intent.ACTION_VIEW
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
-import androidx.appcompat.widget.LinearLayoutCompat
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.ContextCompat.startActivity
-import androidx.recyclerview.widget.DividerItemDecoration
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.storage.FirebaseStorage
-import com.handyopnion.LoadingScreen
-import com.handyopnion.LoadingScreen.hideLoading
-import pl.droidsonroids.gif.GifDrawable
 import java.io.File
 
 class MyAdapter(private val dataArray: ArrayList<DatabaseEvent>): RecyclerView.Adapter<MyViewHolder>(){
@@ -26,9 +17,6 @@ class MyAdapter(private val dataArray: ArrayList<DatabaseEvent>): RecyclerView.A
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val eventRow = layoutInflater.inflate(R.layout.event_row_layout, parent, false)
-
-       /// LoadingScreen.displayLoadingWithText(parent.context,"Please wait...",false)
-
         return MyViewHolder(eventRow)
     }
 
@@ -36,39 +24,12 @@ class MyAdapter(private val dataArray: ArrayList<DatabaseEvent>): RecyclerView.A
         val photo = dataArray[holder.adapterPosition].photo
         holder.name.text=dataArray[holder.adapterPosition].name
         holder.des.text=dataArray[holder.adapterPosition].description
-
-
-        val call = itemCount
-
         val storageRef= FirebaseStorage.getInstance("gs://profeska-ad23d.appspot.com").reference.child("events/$photo")
         val localFile = File.createTempFile("temp","png")
         storageRef.getFile(localFile).addOnSuccessListener {
             val bitmap = BitmapFactory.decodeFile(localFile.absolutePath)
             holder.pic.setImageBitmap(bitmap)
-
-              holder.bar.alpha = 0.toFloat()
-
-            //holder.gif.setVisibility(View.GONE)
-
-         //   if(holder.adapterPosition==call-1){
-          //      hideLoading()
-         //   }
-
-
-
-
         }
-
-
-
-        holder.click.setOnClickListener{
-            Log.d("test","klikanie działa")
-            val myIntent= Intent(holder.itemView.context,EventShowActivity::class.java)
-            myIntent.putExtra("id",photo)
-            startActivity(holder.itemView.context,myIntent,null)
-
-        }
-
 
     }
 
@@ -83,8 +44,4 @@ class MyViewHolder(private val view: View): RecyclerView.ViewHolder(view)
     val name=view.findViewById(R.id.eName) as TextView
     val des=view.findViewById(R.id.eDes) as TextView
     val pic=view.findViewById(R.id.eventImg) as ImageView
-    val click=view.findViewById(R.id.relativeLayout) as ConstraintLayout
-
-    val bar = view.findViewById(R.id.pBar) as ProgressBar
-
 }
